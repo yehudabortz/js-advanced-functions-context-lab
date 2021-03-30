82f5d46c47de522e9c47fafe27d549f65060c9ed
Yehuda Bortz <yehudabortz@icloud.com>
2021-03-30 12:18:52 -0500

    commit tree 9c313342be388594b97ba0d4ceb170ad5d658ca4

--- index.js
+++ index.js
@@ -20,3 +20,68 @@ let allWagesFor = function () {
 
     return payable
 }
+
+function createEmployeeRecord(array) {
+    return {
+        firstName: array[0],
+        familyName:  array[1],
+        title: array[2],
+        payPerHour: array[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(arrays) {
+    return arrays.map(function(array) {
+        return createEmployeeRecord(array)
+    })
+}
+
+function createTimeInEvent(dateStamp) {
+    let date = dateStamp.split(' ')
+    this.timeInEvents.push({
+        type: "TimeIn",
+        hour: parseInt(date[1], 10),
+        date: date[0]
+    })
+    return this
+}
+
+function createTimeOutEvent(dateStamp) {
+    let date = dateStamp.split(' ')
+    this.timeOutEvents.push({
+        type: "TimeOut",
+        hour: parseInt(date[1], 10),
+        date: date[0]
+    })
+    return this
+}
+
+function hoursWorkedOnDate(dateString) {
+    let timeIn = this.timeInEvents.find(function(time) {
+        return time.date === dateString
+    })
+    let timeOut = this.timeOutEvents.find(function(time) {
+        return time.date === dateString
+    })
+    return (timeOut.hour - timeIn.hour) / 100
+}
+
+function wagesEarnedOnDate(dateString) {
+    let wages = hoursWorkedOnDate.call(this, dateString)
+    return wages * this.payPerHour    
+}
+
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(function (array) {
+        return array.firstName === firstName
+    })
+}
+
+function calculatePayroll(srcArray) {
+    return srcArray.reduce(function(payroll, array) {
+        return payroll + allWagesFor.call(array)
+    }, 0)
+}
